5618289f3a65a86d49c7f5e8a837c848968af012
PHKhande <phk151raj@gmail.com>
2022-08-29 10:01:36 +0530

    DOM Manipulation Task 8

--- dom.js
+++ dom.js
@@ -1,173 +1,51 @@
-//git remote add origin https://github.com/PHKhande/SharpenerTasks.git
-
-//console.dir(document);
-
-// console.log(document.domain);
-// console.log(document.URL);
-// console.log(document.title);
-
 document.title = "Hotel Items";
 
-// console.log(document.doctype);
-// console.log(document.head);
-// console.log(document.body);
-// console.log(document.all);
+var form = document.getElementById('addForm');
+var itemList = document.getElementById('items');
 
-//document.all[18].textContent = "Hello, here is your menu list";
+//Form submit event
+form.addEventListener('submit', addItem);
 
-// console.log(document.forms);
-// console.log(document.links);
+//Delete Event
+itemList.addEventListener('click', removeItem);
 
+function addItem(e){
+    e.preventDefault();
+    
+    //Get input value
+    var newItem = document.getElementById('item').value;
 
-//GETELEMENTBYID
-// var header = document.getElementById('main-header');
-// header.style.borderBottom = 'solid 3px #000';
+    //Create new li element
+    var li = document.createElement('li');
 
+    //Add class
+    li.className = 'list-group-item';
+    console.log(li);
 
-// //GETELEMENTBYCLASSNAME
-// var title = document.getElementsByClassName('title');
-// console.log(title);
-// title[0].style.color = 'green';
-// title[0].style.fontWeight = 'bold';
+    //Append the textNode with inp value from form
+    li.appendChild(document.createTextNode(newItem));
 
-// var items = document.getElementsByClassName('list-group-item');
-// items[2].style.backgroundColor = 'green';
+    //Create delete button
+    var delBtn = document.createElement('button');
+    delBtn.className = 'btn btn-danger btn-sm float-right delete';
+    delBtn.appendChild(document.createTextNode('X'));
+    li.appendChild(delBtn);
+    //console.log(delBtn);
 
-// //Gives Error
-// //items[i].style.fontWeight = 'bold';
+    var editBtn = document.createElement('button');
+    editBtn.className = 'btn btn-danger btn-sm float-right delete';
+    editBtn.appendChild(document.createTextNode('Edit'));
+    li.appendChild(editBtn);
 
-// for (let i=0; i < items.length; i++){
-//     items[i].style.fontWeight = 'bold';
-// }
-
-
-// var items = document.getElementsByClassName('list-group-item');
-// for (let i=0; i < items.length; i++){
-//     items[i].style.color = 'skyblue';
-// }
-
-// //GETELEMENTBYTAGNAME
-// var li = document.getElementsByTagName('li');
-// console.log('li');
-
-// for (let i=0; i < li.length; i++){
-//     li[i].style.color = 'skyblue';
-// }
-
-// var items = document.getElementsByClassName('list-group-item');
-// for (let i=0; i < items.length; i++){
-//     items[i].style.fontWeight = 'bold';
-//     items[i].style.backgroundColor = 'green';
-// }
+    itemList.appendChild(li);
+}
 
 
-//QUERYSELECTOR
-// var header = document.querySelector('#main-header');
-// header.style.borderBottom = 'solid 4px #000';
-
-// var input = document.querySelector('input');
-// input.value = 'Hello World!';
-// // Even though it has two inputs, it will select the first one by default
-
-// var submit = document.querySelector('input[type = "submit"]');
-// submit.value = 'SEND';
-
-// var items = document.querySelector('.list-group-item');
-// items.style.color = 'red';
-
-// var secondItem = document.querySelector('.list-group-item:nth-child(2)');
-// secondItem.style.backgroundColor = 'green';
-
-// var lastItem = document.querySelector('.list-group-item:last-child');
-// console.log(lastItem)
-// lastItem.style.backgroundColor = 'red';
-
-// var thirdItem = document.querySelector('.list-group-item:nth-child(3)');
-// thirdItem.style.visibity = 'hidden';
-
-//QUERYSELECTORALL
-// var odd = document.querySelectorAll('li:nth-child(odd)');
-// //console.log(odd);
-// for (var i = 0; i < odd.length; i++){
-//     odd[i].style.backgroundColor = 'green';
-// }
-
-// var second = document.querySelectorAll('li');
-// console.log(second);
-// second[2].style.color = 'red';
-
-
-// TRAVERSING THE DOM //
-
-var itemList = document.querySelector('#items');
-
-// //ParentNode 
-// console.log(itemList.parentNode);
-// itemList.parentNode.style.backgroundColor = 'grey';
-
-//ParentElement
-//console.log(itemList.parentElement);
-itemList.parentElement.style.backgroundColor = 'grey';
-
-//childNodes
-//console.log(itemList.childNodes);
-
-//children
-//console.log(itemList.children);
-itemList.children[0].style.backgroundColor = 'brown';
-
-
-//lastChild
-//console.log(itemList.lastChild);
-
-//lastElementChild
-//console.log(itemList.lastElementChild);
-itemList.lastElementChild.textContent = 'Paneer';
-itemList.firstElementChild.textContent = 'Rice';
-
-// //nextSibling
-// console.log(itemList.nextSibling);
-// console.log(itemList.nextElementSibling);
-
-// console.log(itemList.previousSibling);
-// console.log(itemList.previousElementSibling);
-itemList.previousElementSibling.style.color = 'green';
-
-//createElement
-
-// Create a div
-var newDiv = document.createElement('div');
-
-
-// Add class and id
-newDiv.className = 'hello';
-newDiv.id = 'hello1';
-
-// Add attr
-newDiv.setAttribute('title', 'Hello Div');
-
-
-//Create a text node
-var newDivtext = document.createTextNode('Hello World!');
-
-//Add text node in div
-newDiv.appendChild(newDivtext);
-
-// Adding div inside DOM
-var container = document.querySelector('header .container');
-var h1 = document.querySelector('header h1');
-//console.log(h1);
-
-container.insertBefore(newDiv, h1);
-newDiv.style.fontSize = '20px';
-
-// Adding hello before item1
-var head1 = document.querySelector('div .list-group');
-console.log(head1);
-var list1 = document.querySelector('ul .list-group-item');
-console.log(list1);
-
-head1.insertBefore(newDiv, list1);
-
-
-console.log(newDiv);
+function removeItem(e) {
+    if (e.target.classList.contains('delete')){
+        if(confirm('Are you sure?')){
+            var li = e.target.parentElement
+            itemList.removeChild(li);
+        }
+    }
+}
