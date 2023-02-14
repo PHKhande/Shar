1a9033ce0cb8780f97ca1c994a172c0f1be3f500
PHKhande <phk151raj@gmail.com>
2023-02-14 13:40:41 +0530

    Task6

--- Second/DomMan/dom.js
+++ Second/DomMan/dom.js
@@ -1,9 +1,9 @@
-var mainHeader = document.getElementById('main-header');
-mainHeader.style.border = 'solid 3px #000'
+// var mainHeader = document.getElementById('main-header');
+// mainHeader.style.border = 'solid 3px #000'
 
-var additem = document.getElementsByClassName('title')[0]
-additem.style.fontWeight = 'bold';
-additem.style.color = 'green';
+// var additem = document.getElementsByClassName('title')[0]
+// additem.style.fontWeight = 'bold';
+// additem.style.color = 'green';
 
 // var allitems = document.getElementsByClassName('list-group-item');
 // allitems[2].style.backgroundColor = 'green';
@@ -12,9 +12,17 @@ additem.style.color = 'green';
 //     allitems[i].style.fontWeight = 'bold';
 // }
 
-var liitems = document.getElementsByTagName('li');
-liitems[2].style.backgroundColor = 'green';
+var item2 = document.querySelector('.list-group-item:nth-child(2)');
+var item3 = document.querySelector('.list-group-item:nth-child(3)');
 
-for (i = 0; i < liitems.length; i++) {
-    liitems[i].style.fontWeight = 'bold';
+item2.style.backgroundColor = 'green';
+// item3.style.display = 'None';
+
+var items = document.querySelectorAll('.list-group-item');
+items[1].style.color = 'red';
+
+var odditems = document.querySelectorAll('.list-group-item:nth-child(odd)');
+for (i = 0; i < odditems.length; i++){
+    odditems[i].style.backgroundColor = 'green';
 }
+
