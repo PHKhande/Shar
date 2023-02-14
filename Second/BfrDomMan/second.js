4ac1da04106e96059cab3fb0d94c9347bb89269c
PHKhande <phk151raj@gmail.com>
2023-02-14 10:37:54 +0530

    Second Files

---
+++ Second/BfrDomMan/second.js
@@ -0,0 +1,8 @@
+
+function submitfunc(event){
+    console.log('Name:' + document.getElementById('name').value)
+    console.log('Email:' + document.getElementById('email').value)
+    console.log('Phn:' + document.getElementById('phone').value)
+    console.log('Date:' + document.getElementById('date').value)
+    console.log('Time:' + document.getElementById('time').value)
+}
