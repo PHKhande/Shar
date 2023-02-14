b5914fa2cdd413474a0d7589507053f71297c399
PHKhande <phk151raj@gmail.com>
2023-02-14 13:00:13 +0530

    Task5

--- Second/DomMan/dom.js
+++ Second/DomMan/dom.js
@@ -5,9 +5,16 @@ var additem = document.getElementsByClassName('title')[0]
 additem.style.fontWeight = 'bold';
 additem.style.color = 'green';
 
-var allitems = document.getElementsByClassName('list-group-item');
-allitems[2].style.backgroundColor = 'green';
+// var allitems = document.getElementsByClassName('list-group-item');
+// allitems[2].style.backgroundColor = 'green';
 
-for (i = 0; i < allitems.length; i++) {
-    allitems[i].style.fontWeight = 'bold';
+// for (i = 0; i < allitems.length; i++) {
+//     allitems[i].style.fontWeight = 'bold';
+// }
+
+var liitems = document.getElementsByTagName('li');
+liitems[2].style.backgroundColor = 'green';
+
+for (i = 0; i < liitems.length; i++) {
+    liitems[i].style.fontWeight = 'bold';
 }
