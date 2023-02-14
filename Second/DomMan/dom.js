808e9dbafd1702dd1d6baba1e22ef45794c67fc0
PHKhande <phk151raj@gmail.com>
2023-02-14 12:46:46 +0530

    Task3

---
+++ Second/DomMan/dom.js
@@ -0,0 +1,6 @@
+var mainHeader = document.getElementById('main-header');
+mainHeader.style.border = 'solid 3px #000'
+
+var additem = document.getElementsByClassName('title')[0]
+additem.style.fontWeight = 'bold';
+additem.style.color = 'green';
