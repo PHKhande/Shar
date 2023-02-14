6455a05df2ae6f98f59fe1a9dc66fb57e79566ff
PHKhande <phk151raj@gmail.com>
2023-02-14 12:54:20 +0530

    Task4

--- Second/DomMan/dom.js
+++ Second/DomMan/dom.js
@@ -4,3 +4,10 @@ mainHeader.style.border = 'solid 3px #000'
 var additem = document.getElementsByClassName('title')[0]
 additem.style.fontWeight = 'bold';
 additem.style.color = 'green';
+
+var allitems = document.getElementsByClassName('list-group-item');
+allitems[2].style.backgroundColor = 'green';
+
+for (i = 0; i < allitems.length; i++) {
+    allitems[i].style.fontWeight = 'bold';
+}
