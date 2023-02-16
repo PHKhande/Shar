c8a4b9cb8806a81af43397506a82a5b213d0bb60
PHKhande <phk151raj@gmail.com>
2023-02-16 23:42:22 +0530

    Task13

--- Second/BfrDomMan/second.js
+++ Second/BfrDomMan/second.js
@@ -1,15 +1,82 @@
-// var items = [];
+
+
+var subBtn =  document.getElementById("buttonSubmit");
+subBtn.addEventListener('click',submitfunc);
+
 
 function submitfunc(e){
+    e.preventDefault();
+    const Name = document.getElementById('name').value
+    const Email = document.getElementById('email').value
+    const Phn = document.getElementById('phone').value
+    const Date = document.getElementById('date').value
+    const Time = document.getElementById('time').value
 
-    let infoObj = {
-        'Name' : document.getElementById('name').value,
-        'Email': document.getElementById('email').value,
-        'Phn': document.getElementById('phone').value,
-        'Date': document.getElementById('date').value,
-        'Time': document.getElementById('time').value
+    const obj = {
+        Name,
+        Email,
+        Phn,
+        Date,
+        Time
     }
-    let objStr = JSON.stringify(infoObj);
-    // items.push(objStr)
-    localStorage.setItem(document.getElementById('email').value, objStr);
+    
+    localStorage.setItem(obj.Email, JSON.stringify(obj));
+    showUserOnScreen(obj);
+
+
+function showUserOnScreen(obj){
+    var parentElem = document.getElementById('listOfUsers');
+    var newli = document.createElement('li');
+    newli.textContent = obj.Name + " " + obj.Email + " " + obj.Phn + " " + obj.Date +  " " + obj.Time
+
+    var newBtn = document.createElement('button');
+    newBtn.id = 'btn';
+    newBtn.className = 'btn btn-danger delete';
+    newBtn.appendChild(document.createTextNode("Delete"));
+    newli.appendChild(newBtn)
+    parentElem.appendChild(newli)
+
+    newBtn.onclick = () => {
+        localStorage.removeItem(obj.Email);
+        parentElem.removeChild(newli)
+    }
 }
+}
+
+
+
+
+
+
+
+//     var newBtn = document.createElement('button');
+//     newBtn.id = 'btn';
+//     newBtn.className = 'btn btn-danger delete';
+//     newBtn.appendChild(document.createTextNode("Delete"));
+    
+//     newli.appendChild(document.createTextNode(document.getElementById('name').value  + " "  ));
+//     var email = document.createTextNode(document.getElementById('email').value)
+//     newli.appendChild(email);
+//     newli.appendChild(document.createTextNode(" " + document.getElementById('phone').value + " "));
+//     newli.appendChild(newBtn);
+
+//     newDiv.childNodes[1].appendChild(newli);
+
+
+//     var lidel = document.getElementById('infoli');
+//     lidel.addEventListener('click', deleteItem);
+//     var ul = document.querySelector('#newul');
+    
+//     function deleteItem(e){
+//         if(e.target.classList.contains('delete')){
+//             var li = e.target.parentElement;
+//             console.log(li.childNodes)
+//             newDiv.childNodes[1].removeChild(li);
+//             localStorage.removeItem('li.childNodes');
+
+//         }
+//     }
+    
+// }
+
+
