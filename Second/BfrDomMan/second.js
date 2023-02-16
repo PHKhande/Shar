014fbefaa5fe3d9f3d21881429a1977326dde181
PHKhande <phk151raj@gmail.com>
2023-02-16 15:42:26 +0530

    Task10

--- Second/BfrDomMan/second.js
+++ Second/BfrDomMan/second.js
@@ -1,8 +1,15 @@
 
-function submitfunc(event){
+function submitfunc(e){
     console.log('Name:' + document.getElementById('name').value)
     console.log('Email:' + document.getElementById('email').value)
     console.log('Phn:' + document.getElementById('phone').value)
     console.log('Date:' + document.getElementById('date').value)
     console.log('Time:' + document.getElementById('time').value)
+
+    sessionStorage.setItem('Name', document.getElementById('name').value)
+    sessionStorage.setItem('Email', document.getElementById('email').value)
+    sessionStorage.setItem('Phn', document.getElementById('phone').value)
+    sessionStorage.setItem('Date', document.getElementById('date').value)
+    sessionStorage.setItem('Time', document.getElementById('time').value)
+
 }
