36d790af98162066e1e873d36f12adae7ffda8d2
PHKhande <phk151raj@gmail.com>
2023-02-22 00:12:55 +0530

    ExpenseTracker

---
+++ Second/ExpenseTracker/expense.js
@@ -0,0 +1,57 @@
+
+var subBtn =  document.getElementById("btnSubmit");
+subBtn.addEventListener('click',submitfunc);
+
+function submitfunc(e) {
+    e.preventDefault();
+    const Amount = document.getElementById('expenseAmount').value
+    const Category= document.getElementById('expenseCategory').value
+    const Description = document.getElementById('expenseDescription').value
+
+    const obj = {
+        Amount,
+        Category,
+        Description
+    }
+
+    localStorage.setItem(obj.Amount, JSON.stringify(obj));
+    expenseDetails(obj);
+
+    function expenseDetails(obj){
+        var parentElem = document.getElementById('expenseDetailsUl');
+        var newli = document.createElement('li');
+        newli.textContent = obj.Category + "-" + obj.Description + "-" + obj.Amount;
+        newli.className = "list-group-item";
+
+        var editBtn = document.createElement('button');
+        editBtn.id = 'edtBtn';
+        editBtn.className = 'btn btn-success edit';
+        editBtn.appendChild(document.createTextNode(" Edit Expense "));
+        newli.appendChild(editBtn)
+
+        var delBtn = document.createElement('button');
+        delBtn.id = 'DelBtn';
+        delBtn.className = 'btn btn-danger delete';
+        delBtn.appendChild(document.createTextNode(" Delete Expense "));
+        newli.appendChild(delBtn)
+
+        parentElem.appendChild(newli);
+
+        editBtn.onclick = () => {
+            let editObj = JSON.parse(localStorage.getItem(obj.Amount));
+
+            document.getElementById('expenseAmount').value = editObj.Amount
+            document.getElementById('expenseCategory').value = editObj.Category
+            document.getElementById('expenseDescription').value = editObj.Description
+
+            localStorage.removeItem(obj.Amount);
+            parentElem.removeChild(newli);
+        }
+
+        delBtn.onclick = () => {
+            localStorage.removeItem(obj.Amount);
+            parentElem.removeChild(newli);
+        }
+
+    }
+}
