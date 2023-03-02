11d44f757fa622a79f8f7521f5c6c8a481e122de
PHKhande <phk151raj@gmail.com>
2023-03-02 19:33:09 +0530

    Task01 Starting with Node.js

--- Second/SellerAdminPage/index.js
+++ Second/SellerAdminPage/index.js
@@ -1,7 +1,7 @@
 var z = 0
 window.addEventListener('DOMContentLoaded', async () => {
     try{
-        let response = await axios.get("https://crudcrud.com/api/9d26443d457c4316bdeca2d9338b9cc7/productsData")
+        let response = await axios.get("https://crudcrud.com/api/c380ba9ebe1d4dca87ef515264996cab/productsData")
         for (let i = 0; i < response.data.length; i++){
             showProductdetails(response.data[i]);
             let a = response.data[i].totalValue ? parseInt(response.data[i].totalValue) : 0
@@ -9,7 +9,9 @@ window.addEventListener('DOMContentLoaded', async () => {
         }
         showTotalValue(z,0);
 
-    } catch(err){
+    } 
+    
+    catch(err){
         console.log(err);
         document.body.innerHTML += `<h2> Something went wrong</h2>`
     }
@@ -39,10 +41,10 @@ async function addProduct(e){
             totalValue
         }
 
-        let response = await axios.post("https://crudcrud.com/api/9d26443d457c4316bdeca2d9338b9cc7/productsData", obj)
-            console.log(response)
-            showProductdetails(response.data);
-            showTotalValue(parseInt(temp), response.data.totalValue);
+        let response = await axios.post("https://crudcrud.com/api/c380ba9ebe1d4dca87ef515264996cab/productsData", obj)
+        console.log(response)
+        showProductdetails(response.data);
+        showTotalValue(parseInt(temp), response.data.totalValue);
 
     } catch (err){
         console.log(err);
@@ -78,7 +80,7 @@ function showProductdetails(obj){
             document.getElementById('sellingPrice').value = obj.sellingPrice
             document.getElementById('productName').value = obj.productName
 
-            await axios.delete(`https://crudcrud.com/api/9d26443d457c4316bdeca2d9338b9cc7/productsData/${obj._id}`)
+            await axios.delete(`https://crudcrud.com/api/c380ba9ebe1d4dca87ef515264996cab/productsData/${obj._id}`)
             parentElem.removeChild(newli)
 
             var removeDiv = document.getElementById('productdiv');
@@ -98,7 +100,7 @@ function showProductdetails(obj){
     deleteBtn.onclick = async () => {
 
         try{
-            await axios.delete(`https://crudcrud.com/api/9d26443d457c4316bdeca2d9338b9cc7/productsData/${obj._id}`)
+            await axios.delete(`https://crudcrud.com/api/c380ba9ebe1d4dca87ef515264996cab/productsData/${obj._id}`)
 
             parentElem.removeChild(newli);
             var removeDiv = document.getElementById('productdiv');
@@ -125,7 +127,6 @@ function showTotalValue(smallValue, totalValue){
     ptotal.className = "d-inline-block"
     ptotal.id = 'removeChi'
     let lastValue = smallValue +  totalValue
-    
     ptotal.textContent = lastValue
     
     parentparaElem.appendChild(ptotal)
