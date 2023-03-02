fce02ea02d1d2e19b95012138547a468f1cfbc63
PHKhande <phk151raj@gmail.com>
2023-03-02 21:04:08 +0530

    Task03 Arrays , Spread and Rest Operators

--- NodeJSCompleteGuide/firstApp.js
+++ NodeJSCompleteGuide/firstApp.js
@@ -1,16 +1,25 @@
-console.log("hello world");
+// console.log("hello world");
 
-const abc = (a, b) => a * b
-console.log(abc(5, 6))
+// const abc = (a, b) => a * b
+// console.log(abc(5, 6))
 
-//Creating Objects
-const student = {
-    name : 'Prathviraj',
-    age : 24,
-    class : 'Backend',
-    studies() {
-        console.log(`Studies ${this.class}`)
-    } 
-}
+// //Creating Objects
+// const student = {
+//     name : 'Prathviraj',
+//     age : 24,
+//     class : 'Backend',
+//     studies() {
+//         console.log(`Studies ${this.class}`)
+//     } 
+// }
+// student.studies()
 
-student.studies()
+const arr = [ 'apple', 'oranges', ' ', 'mango', ' ', 'lemon']
+console.log(arr.map( fruit => {
+    if(fruit == ' '){
+        return 'empty string'
+    }
+    else return fruit
+}))
+
+
