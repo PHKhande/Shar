df0433574848c310bfa378c821c1a228c1cf2d55
PHKhande <phk151raj@gmail.com>
2023-03-02 20:05:04 +0530

    Task02 Revise JS Concepts

--- NodeJSCompleteGuide/firstApp.js
+++ NodeJSCompleteGuide/firstApp.js
@@ -1,1 +1,16 @@
 console.log("hello world");
+
+const abc = (a, b) => a * b
+console.log(abc(5, 6))
+
+//Creating Objects
+const student = {
+    name : 'Prathviraj',
+    age : 24,
+    class : 'Backend',
+    studies() {
+        console.log(`Studies ${this.class}`)
+    } 
+}
+
+student.studies()
