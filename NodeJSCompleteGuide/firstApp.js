fccb815d1972982956f8fc8fdef4e520a9b7ec44
PHKhande <phk151raj@gmail.com>
2023-03-03 21:47:19 +0530

    Task08 Writing & Reading from a file

--- NodeJSCompleteGuide/firstApp.js
+++ NodeJSCompleteGuide/firstApp.js
@@ -1,17 +1,35 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer( (req, res) => {
-    if (req.url == '/home'){
-        res.write('<html><body><h2>Welcome home</h2></body></html>')
-        res.end()
+    const url = req.url;
+    const method = req.method;
+    if (url === '/'){
+        fs.readFile('message.txt', 'utf8', (err, data) => {
+                const printData = data;
+                res.write(`<html><head><title>Enter User Details</title></head><body><header><div><h2>${printData}<h2></div></header><form action="/message" method="POST"><input type="text" name="valueMessage"></input><button type="submit">Submit</button></form></body></html>`);
+                res.end();
+        });
     }
-    else if (req.url == '/about'){
-        res.write('<html><body><h2>Welcome to About Us page</h2></body></html>')
-        res.end()
-    }
-    else if (req.url == '/node'){
-        res.write('<html><body><h2>Welcome to my Node Js project</h2></body></html>')
-        res.end()
+
+    if (url === '/message' && method === 'POST'){
+        const body = [];
+        req.on('data', (chunk) => {
+            body.push(chunk)
+        })
+        return req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString()
+            //Above line will return valueMessage=whole message we write, therefore we split it
+            const message = parsedBody.split('=')[1]
+            fs.writeFile("message.txt", message, err => {
+                //If any error occurs, then it will return an error(err)
+                res.statusCode = 302
+                res.setHeader("Location" , '/')
+                res.end()
+            })
+            
+        })
+        
     }
 });
 
