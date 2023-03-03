d9e2e1eaae267b8b563f161029007abc56ace538
PHKhande <phk151raj@gmail.com>
2023-03-03 17:48:41 +0530

    Task07 Response/Request

--- NodeJSCompleteGuide/firstApp.js
+++ NodeJSCompleteGuide/firstApp.js
@@ -1,7 +1,18 @@
 const http = require('http');
 
 const server = http.createServer( (req, res) => {
-    console.log('Prathviraj Khande')
+    if (req.url == '/home'){
+        res.write('<html><body><h2>Welcome home</h2></body></html>')
+        res.end()
+    }
+    else if (req.url == '/about'){
+        res.write('<html><body><h2>Welcome to About Us page</h2></body></html>')
+        res.end()
+    }
+    else if (req.url == '/node'){
+        res.write('<html><body><h2>Welcome to my Node Js project</h2></body></html>')
+        res.end()
+    }
 });
 
-server.listen(4000, (res) => console.log(res))
+server.listen(4000)
