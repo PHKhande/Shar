94bb324f9730b6ac254438072068c7c29ad54197
PHKhande <phk151raj@gmail.com>
2023-03-05 15:00:20 +0530

    Task03 Intro to Middlewares

--- NodeJSCompleteGuide/firstApp.js
+++ NodeJSCompleteGuide/firstApp.js
@@ -1,15 +1,22 @@
-const express = require('express')
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const app = express()
+const app = express();
 
-app.use( (req, res, next) => {
-    console.log('Middleware 1');
-    next();
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use('/add-product', (req, res, next) => {
+    res.send(`<html><head><title>Add Product Details</title></head><body><form action="/product" method="POST"><input type="text" name="product"></input><br><br><input type="text" name="productSize"></input><br><br><button type="submit">Add Product</button></form></body></html>`)
 });
 
-app.use( (req, res, next) => {
-    console.log('Middleware 2');
-    res.send('<h1>Hello!!</h1>')
+app.post('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/')
 });
 
-app.listen(4000)
+app.use('/', (req, res, next) => {
+    console.log('Hello')
+    res.send('<h1> Hello!!! </h1>')
+});
+
+app.listen(4000);
